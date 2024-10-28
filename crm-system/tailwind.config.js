a9b84beae6f73051c683fdab65801282aac109d7
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-10-28 12:00:45 +0300

    setup project

---
+++ crm-system/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ["cairo", "sans-serif"],
+    },
+  },
+  daisyui: {
+    themes: [],
+  },
+  plugins: [require("daisyui")],
+};
