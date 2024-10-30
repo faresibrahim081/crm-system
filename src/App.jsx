56aeed6d8d3f2dcec82277b90fcbbc03e1865bb4
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-10-30 13:06:40 +0300

    SideBar-and-File-Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import SideBar from "./Components/SideBar";
 
 function App() {
- 
-
   return (
     <>
-      
+      <SideBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
