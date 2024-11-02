7bcf6412a2a7b879a9bf96093e407f909e7320d4
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-11-02 14:23:30 +0200

    Register-Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import Register from './Components/Register/Register'
+import { useState } from "react";
+import "./App.css";
+import Register from "./Components/Forms/Register/Register";
 import "./App.css";
 import SideBar from "./Components/SideBar";
 import Home from "./Pages/Home";
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       {/* <SideBar/> */}
-      <Home/>
-      {/* <Register/> */}
+      {/* <Home/> */}
+      {/* <Register /> */}
     </>
   );
 }
