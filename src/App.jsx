e74b7ed9024d8805bb0334d56ad686e9a19d0aec
Amr Atta <amribrahimatta@gmail.com>
2024-11-02 12:17:50 +0200

    Final Register Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-import DashboardHead from './Components/DashboardHead'
 import Register from './Components/Register/Register'
 import "./App.css";
-import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
       <Register/>
-      <Home />
     </>
   );
 }
