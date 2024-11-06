e0a4864c94ee8218d2655922726784392a049902
Amr Atta <amribrahimatta@gmail.com>
2024-11-06 15:37:30 +0200

    finished and linked Register, User Login and admin Login with homepage using routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import "./App.css";
-import Register from "./Components/Forms/Register/Register";
-import Home from "./Pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Router } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Sign from './Components/Forms/Sign/Sign';
+import User from './Components/Forms/User/User';
+import Register from './Components/Forms/Register/Register';
 
 function App() {
   return (
     <div className="bg-[#292929]">
-      <Router>
+      {/* <Router> */}
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign" element={<Sign />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
-      </Router>
+      {/* </Router> */}
     </div>
   );
 }
