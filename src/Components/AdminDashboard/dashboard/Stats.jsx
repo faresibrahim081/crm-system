84f993aaf7277c08fe6ab35e2585891b8937529b
Fares ibrahim <faresibrahim081@gmail.com>
2024-11-18 15:25:16 +0200

    the end of graphs

--- src/Components/AdminDashboard/dashboard/Stats.jsx
+++ src/Components/AdminDashboard/dashboard/Stats.jsx
@@ -27,11 +27,13 @@ const DashboardHead = () => {
   ];
 
   return (
+    <>
+    <h1 className="font-bold text-[24px] text-right text-white">لوحة التحكم</h1>
     <div className="flex flex-col items-center justify-center md:flex-row rounded-">
       {stats.map((stat, index) => (
         <div
           key={index}
-          className="flex items-center justify-around w-screen p-6 space-x-4 bg-[#0d0d0d] shadow"
+          className="flex items-center justify-around w-full p-6 space-x-4 bg-[#0d0d0d] shadow"
         >
           <div className="flex items-center justify-center w-16 h-16 bg-[linear-gradient(201.18deg,_#5CBF8A_3.14%,_#057639_86.04%)] rounded-full">
             {stat.icon}
@@ -43,6 +45,7 @@ const DashboardHead = () => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 
